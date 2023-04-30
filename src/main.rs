3ed1b9a0c34f79eb2093c6cb58c7bbb8fa0b69b0
nalabrie <nalabrie@student.ysu.edu>
2023-04-30 15:15:23 -0400

    Does VERY primitive URL validation (trying to avoid regex so the binary doesn't bloat)

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,60 @@ use std::time::Duration;
 
 use clipboard_win::{get_clipboard_string, set_clipboard_string};
 
+// ---- GLOBAL CONSTANTS ----
+
+// how long to wait between clipboard reads
+const WAIT_DURATION: Duration = Duration::from_millis(250);
+
+// primitive "regex alternative" to cover most cases when validating URLs
+const URL_VALIDATION_ARRAY: [&str; 17] = [
+    "http", "www.", ".com", ".org", ".net", ".edu", ".gov", ".info", ".io", ".biz", ".pro", ".xzy",
+    ".de", ".uk", ".top", ".cn", ".tk",
+];
+
+// ---- FUNCTIONS ----
+
+/// Checks if a String is a valid URL. Returns `true` when valid.
+///
+/// # Arguments
+///
+/// * `url`: String reference of URL to be checked for a valid format.
+///
+/// returns: bool
+///
+/// # Examples
+///
+/// ```
+///  if validate_url(my_url_string) {
+///      /* code to execute when valid URL is found */
+///  }
+/// ```
+fn validate_url(url: &String) -> bool {
+    URL_VALIDATION_ARRAY.iter().any(|&s| url.contains(s))
+}
+
+// ---- MAIN ----
+
 fn main() -> Result<(), Error> {
     let mut result: String;
     let mut prev_result = String::new();
-    let wait_duration = Duration::from_millis(250);
     set_clipboard_string("").expect("Clearing clipboard on first run");
 
     loop {
         result = get_clipboard_string().unwrap_or(prev_result.clone());
 
-        if result == prev_result {
-            sleep(wait_duration);
+        if result == prev_result || !validate_url(&result) {
+            sleep(WAIT_DURATION);
             continue;
         }
 
         prev_result = result.clone();
         println!("Now playing: {result}");
-        Command::new("mpv.exe").arg("--mute").arg(result).spawn()?;
+        Command::new("mpv.exe")
+            .arg("--mute")
+            .arg(result)
+            .spawn()
+            .expect("Launch mpv with clipboard URL");
     }
 }
+// sample URL: https://youtu.be/9FLRHejWAo8
